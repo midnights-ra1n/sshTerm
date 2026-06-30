a29de2e2c95dcac6485666c271c6ebbb9806ae2d
midnights-ra1n <noe@ngrt.fr>
2026-06-30 18:56:21 +0200

    Persistence function now implemented

--- sshTerm/Models/SidebarItem.swift
+++ sshTerm/Models/SidebarItem.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum SidebarItem: Identifiable, Hashable {
+enum SidebarItem: Identifiable, Hashable, Codable {
     case connection(SSHConnection)
     case folder(SSHFolder)
 
@@ -36,6 +36,39 @@ enum SidebarItem: Identifiable, Hashable {
         switch self {
         case .folder: return true
         case .connection: return false
+        }
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case type, connection, folder
+    }
+
+    private enum ItemType: String, Codable {
+        case connection, folder
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let type = try container.decode(ItemType.self, forKey: .type)
+        switch type {
+        case .connection:
+            let connection = try container.decode(SSHConnection.self, forKey: .connection)
+            self = .connection(connection)
+        case .folder:
+            let folder = try container.decode(SSHFolder.self, forKey: .folder)
+            self = .folder(folder)
+        }
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case .connection(let connection):
+            try container.encode(ItemType.connection, forKey: .type)
+            try container.encode(connection, forKey: .connection)
+        case .folder(let folder):
+            try container.encode(ItemType.folder, forKey: .type)
+            try container.encode(folder, forKey: .folder)
         }
     }
 }
